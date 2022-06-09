beddd1ccb4ee88de82cb24624ca3b22dc4bd3711
FurqatMashrabjonov <php_lesson@mail.ru>
2022-06-09 10:46:29 +0500

    Changed by Furat

---
+++ db.js
@@ -0,0 +1,16 @@
+const { Pool } = require('pg')
+
+const pool = new Pool({
+    host: 'localhost',
+    port: 5432,
+    user: 'postgres',
+    password: 'postgres',
+    database: 'pg'
+})
+
+
+const client = pool.client()
+
+const {rows} = client.query('select * from users')
+client.release()
+console.log(rows)
